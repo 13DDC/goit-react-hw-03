e6bcf7349a118cd2f9da13f01eb3c87acc4e8726
13DDC <150695013+13DDC@users.noreply.github.com>
2024-07-11 22:46:58 +0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import Contact from "./Contact/Contact";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
 
+const contact = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <ContactForm />
+      <SearchBox />
+      <ContactList Contact={contact} />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/SearchBox/SearchBox.jsx
@@ -0,0 +1,6 @@
+import s from "./SearchBox.module.css";
+const SearchBox = () => {
+  return <div>SearchBox</div>;
+};
+
+export default SearchBox;
